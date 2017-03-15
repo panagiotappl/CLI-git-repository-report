d592d0abf7fbe8cec1052a5efb48c4042271a4b2
panagiotappl <panagiotappl@gmail.com>
2017-03-16 01:53:20 +0200

    Reboot2

--- src/cli.py
+++ src/cli.py
@@ -3,77 +3,69 @@ import sys
 import re
 import subprocess
 
-def number_of_files():
-    command = "git ls-files"
+
+def execute_command(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    rows = process.readlines()
+    result = process.readlines()
     process.close()
 
-    return len(rows)
+    return result
+
+
+def number_of_files():
+    results = execute_command("git ls-files")
+    return len(results)
+
 
 def number_of_lines():
-    command = "git ls-files | xargs wc -l"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    rows = process.readlines()
+    results = execute_command("git ls-files | xargs wc -l")
+    return results[len(results) - 1].strip().split(' ', 1)[0]
 
-    return rows[len(rows) - 1].strip().split(' ', 1)[0]
 
 def committer_stats():
     # Number of committers
-    command = "git log --all --format='%aN' | sort -u "
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    rows = process.readlines()
-    process.close()
+    results = execute_command("git log --all --format='%aN' | sort -u")
 
     commiters = []
-    for j in range(0, len(rows)):
-        commiters.append(rows[j].decode("utf-8", "replace").strip())
+    for j in range(0, len(results)):
+        commiters.append(results[j].decode("utf-8", "replace").strip())
     print "Number of committers: ", len(commiters)
 
     # Number of commits
-    command = "git shortlog --all | grep -E '^[ ]+\w+' | wc -l"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    rows = process.readlines()
-    process.close()
+    results = execute_command("git shortlog --all | grep -E '^[ ]+\w+' | wc -l")
 
-    commits = rows[0]
+    commits = results[0]
     print "Number of commits: ", commits
     print
 
     # Number of insertions/deletions per committer
     print "Number of insertions/deletions per committer:"
-    for i in range(0, len(commiters)):
-        command = 'git log --all --author="' + commiters[i] + '" --oneline --shortstat'
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    for j in range(0, len(commiters)):
+        results = execute_command('git log --all --author="' + commiters[j] + '" --oneline --shortstat')
 
         adds, dels = 0, 0
-        for line in process.readlines():
-            am, dm = re.search(r'\d+(?= insertions)', line), re.search(r'\d+(?= deletions)', line)
+        for i in range(0, len(results)):
+            am, dm = re.search(r'\d+(?= insertions)', results[i]), re.search(r'\d+(?= deletions)', results[i])
             if am is not None:
                 adds += int(am.group())
             if dm is not None:
                 dels += int(dm.group())
-        process.close()
 
-        print commiters[i], ": ", adds, " insertions (+), ",  dels, " deletions(-)"
+        print commiters[j], ": ", adds, " insertions (+), ", dels, " deletions(-)"
     print
 
     # Number of commits per author
-    command = "git shortlog -sn --all"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-
-    rows = process.readlines()
-    process.close()
+    results = execute_command("git shortlog -sn --all")
 
     commits_per_author = {}
     print "Number of commits per author: "
-    for i in range(0, len(rows)):
-        commits_per_author[rows[i].strip().split('\t', 1)[1]] = rows[i].strip().split('\t', 1)[0]
-
+    for i in range(0, len(results)):
+        commits_per_author[results[i].strip().split('\t', 1)[1]] = results[i].strip().split('\t', 1)[0]
 
     for i in commits_per_author:
-        percentage = float(commits_per_author[i])/float(commits) * 100
+        percentage = float(commits_per_author[i]) / float(commits) * 100
         print i, ": ", round(percentage, 2), "%"
+
 
 def main():
     repo_path = sys.argv[1]
@@ -88,5 +80,6 @@ def main():
     committer_stats()
     print
 
+
 if __name__ == "__main__":
-	main()
+    main()
