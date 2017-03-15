5e06b6702be241cb1656d1390fcd8425196c956c
panagiotappl <panagiotappl@gmail.com>
2017-03-16 01:35:26 +0200

    Reboot

--- src/cli.py
+++ src/cli.py
@@ -1,34 +1,92 @@
-from __future__ import division
-
-import git
+import os
 import sys
+import re
+import subprocess
+
+def number_of_files():
+    command = "git ls-files"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    rows = process.readlines()
+    process.close()
+
+    return len(rows)
+
+def number_of_lines():
+    command = "git ls-files | xargs wc -l"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    rows = process.readlines()
+
+    return rows[len(rows) - 1].strip().split(' ', 1)[0]
+
+def committer_stats():
+    # Number of committers
+    command = "git log --all --format='%aN' | sort -u "
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    rows = process.readlines()
+    process.close()
+
+    commiters = []
+    for j in range(0, len(rows)):
+        commiters.append(rows[j].decode("utf-8", "replace").strip())
+    print "Number of committers: ", len(commiters)
+
+    # Number of commits
+    command = "git shortlog --all | grep -E '^[ ]+\w+' | wc -l"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    rows = process.readlines()
+    process.close()
+
+    commits = rows[0]
+    print "Number of commits: ", commits
+    print
+
+    # Number of insertions/deletions per committer
+    print "Number of insertions/deletions per committer:"
+    for i in range(0, len(commiters)):
+        command = 'git log --all --author="' + commiters[i] + '" --oneline --shortstat'
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+
+        adds, dels = 0, 0
+        for line in process.readlines():
+            am, dm = re.search(r'\d+(?= insertions)', line), re.search(r'\d+(?= deletions)', line)
+            if am is not None:
+                adds += int(am.group())
+            if dm is not None:
+                dels += int(dm.group())
+        process.close()
+
+        print commiters[i], ": ", adds, " insertions (+), ",  dels, " deletions(-)"
+    print
+
+    # Number of commits per author
+    command = "git shortlog -sn --all"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+
+    rows = process.readlines()
+    process.close()
+
+    commits_per_author = {}
+    print "Number of commits per author: "
+    for i in range(0, len(rows)):
+        commits_per_author[rows[i].strip().split('\t', 1)[1]] = rows[i].strip().split('\t', 1)[0]
 
 
-#get repository path from command line arguments
-repo = git.Repo(sys.argv[1])
+    for i in commits_per_author:
+        percentage = float(commits_per_author[i])/float(commits) * 100
+        print i, ": ", round(percentage, 2), "%"
 
-print "Number of files: ", len(repo.index.entries)
-print "Number of branches: ", len(repo.branches)
-print "Number of tags: ", len(repo.tags)
+def main():
+    repo_path = sys.argv[1]
+    os.chdir(repo_path)
 
-commits = list(repo.iter_commits())
-print "Number of commits: ", len(commits)
+    file_count = number_of_files()
+    print "Number of files: ", file_count
 
+    line_count_total = number_of_lines()
+    print "Total number of lines: ", line_count_total
 
-authors = list()
-commits_per_author = {}
-for commit in commits:
-    if commit.author not in authors:
-        authors.append(commit.author)
-        commits_per_author[commit.author] = 1
-    else:
-        commits_per_author[commit.author] += 1
+    committer_stats()
+    print
 
-
-print "Number of authors: ", len(authors)
-print commits_per_author
-print len(commits)
-
-print "{:<25} {:<15}".format('Author','% of commits')
-for author, comm in commits_per_author.iteritems():
-    print "{:<25} {:<15}".format(author, str(comm/len(commits) * 100) + "%")
+if __name__ == "__main__":
+	main()
