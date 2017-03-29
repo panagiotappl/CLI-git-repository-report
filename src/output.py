c3a8bb2df6076ad0b8bbb1ff956a1088dfb3c064
Panos <paparrigopoulosp@gmail.com>
2017-03-29 09:18:46 +0300

    Removes remote branches info

--- src/output.py
+++ src/output.py
@@ -435,23 +435,9 @@ def generate_output(statistics, path):
         output_html += "<td>" + str(value) + " (+)</td>\n"
         output_html += "</tr>\n"
     output_html += "</tbody>\n</table>\n</div>\n"
+    #Percentage of commits per author per branch
 
-    output_html += "    <div class=\"row\">\n"
-    output_html += "         <table class=\"table table-striped\">\n"
-    output_html += "    <thead >\n"
-    output_html += "      <tr>\n"
-    output_html += "        <th class=\"header bgteal\" >Remote Branch</th>\n"
-    output_html += "        <th class=\"header bgteal\" >Number of commits</th>\n"
-    output_html += "      </tr>\n"
-    output_html += "    </thead>\n"
-    output_html += " <tbody>\n"
 
-    for key, value in com_branchR.iteritems():
-        output_html += "<tr>\n"
-        output_html += "<td>" + key + "</td>\n"
-        output_html += "<td>" + str(value) + "</td>\n"
-        output_html += "</tr>\n"
-    output_html += "</tbody>\n</table>\n</div>\n"
 
     output_html += "    <div class=\"row \">\n"
     output_html += "        <h2>Percentage of commits per author per branch</h2>\n"
@@ -476,60 +462,60 @@ def generate_output(statistics, path):
             output_html += "<td>" + str(commiter[0]) + "</td>\n"
             output_html += "<td>" + str(commiter[1]) + "%</td>\n"
             output_html += "</tr>\n"
-    output_html += "</tbody>\n</table>\n</div>"#</div>\n"
-
-    output_html += "    <div class=\"row\">\n"
-    output_html += "         <table class=\"table table-striped\">\n"
-    output_html += "    <thead >\n"
-    output_html += "      <tr>\n"
-    output_html += "        <th class=\"header bgbrown\" >Remote Branch</th>\n"
-    output_html += "        <th class=\"header bgbrown\" >Author</th>\n"
-    output_html += "        <th class=\"header bgbrown\" >% of commits</th>\n"
-    output_html += "      </tr>\n"
-    output_html += "    </thead>\n"
-    output_html += " <tbody>\n"
-
-    com_br_authR = statistics["br_stats"]["com_br_authR"]
-    for key, value in com_br_authR.iteritems():
-        for commiter in value:
-            output_html += "<tr>\n"
-            output_html += "<td>" + key + "</td>\n"
-            output_html += "<td>" + str(commiter[0]) + "</td>\n"
-            output_html += "<td>" + str(commiter[1]) + "%</td>\n"
-            output_html += "</tr>\n"
     output_html += "</tbody>\n</table>\n</div></div>\n"
+    #
+    # output_html += "    <div class=\"row\">\n"
+    # output_html += "         <table class=\"table table-striped\">\n"
+    # output_html += "    <thead >\n"
+    # output_html += "      <tr>\n"
+    # output_html += "        <th class=\"header bgbrown\" >Remote Branch</th>\n"
+    # output_html += "        <th class=\"header bgbrown\" >Author</th>\n"
+    # output_html += "        <th class=\"header bgbrown\" >% of commits</th>\n"
+    # output_html += "      </tr>\n"
+    # output_html += "    </thead>\n"
+    # output_html += " <tbody>\n"
+    #
+    # com_br_authR = statistics["br_stats"]["com_br_authR"]
+    # for key, value in com_br_authR.iteritems():
+    #     for commiter in value:
+    #         output_html += "<tr>\n"
+    #         output_html += "<td>" + key + "</td>\n"
+    #         output_html += "<td>" + str(commiter[0]) + "</td>\n"
+    #         output_html += "<td>" + str(commiter[1]) + "%</td>\n"
+    #         output_html += "</tr>\n"
+    # output_html += "</tbody>\n</table>\n</div></div>\n"
 
     output_html += "<div id=\"branches\" class=\"container-fluid slideanim\" align=\"middle\">\n"
-    output_html += "    <div class=\"row \">\n"
-    output_html += "        <h1>Branches</h1>\n"
-    output_html += "         <p>Click on the link of each branch to see its commit history.\n"
-    output_html += "    </div>\n"
-    output_html += "    <div class=\"row \">\n"
-    output_html += "        <h2>Remote Branches</h2>\n"
-    output_html += "    </div>\n"
-    output_html += "    <div class=\"row\">\n"
-    output_html += "         <table class=\"table table-striped\">\n"
-    output_html += "    <thead >\n"
-    output_html += "      <tr>\n"
-    output_html += "        <th class=\"header bglightteal\" >Branch</th>\n"
-    output_html += "        <th class=\"header bglightteal\" >Date created</th>\n"
-    output_html += "        <th class=\"header bglightteal\" >Date modified</th>\n"
-    output_html += "      </tr>\n"
-    output_html += "    </thead>\n"
-    output_html += " <tbody>\n"
+    # output_html += "    <div class=\"row \">\n"
+    # output_html += "        <h1>Branches</h1>\n"
+    # output_html += "         <p>Click on the link of each branch to see its commit history.\n"
+    # output_html += "    </div>\n"
+    # output_html += "    <div class=\"row \">\n"
+    # output_html += "        <h2>Remote Branches</h2>\n"
+    # output_html += "    </div>\n"
+    # output_html += "    <div class=\"row\">\n"
+    # output_html += "         <table class=\"table table-striped\">\n"
+    # output_html += "    <thead >\n"
+    # output_html += "      <tr>\n"
+    # output_html += "        <th class=\"header bglightteal\" >Branch</th>\n"
+    # output_html += "        <th class=\"header bglightteal\" >Date created</th>\n"
+    # output_html += "        <th class=\"header bglightteal\" >Date modified</th>\n"
+    # output_html += "      </tr>\n"
+    # output_html += "    </thead>\n"
+    # output_html += " <tbody>\n"
 
     """
         Remote Branches with dates go here.
     """
-    remoteB = statistics["br_stats"]["remoteB"]
+    #remoteB = statistics["br_stats"]["remoteB"]
     br_stats = statistics["br_stats"]
-    for i in range(0, len(remoteB)):
-        output_html += "<tr>\n"
-        output_html += "<td><a href=\"" + remoteB[i].split("/")[-1] + "_remote.html\">" + remoteB[i] + "</a></td>\n"
-        output_html += "<td>" + str(br_stats['branch_dates_remote'][remoteB[i].strip()][0]) + "</td>\n"
-        output_html += "<td>" + str(br_stats['branch_dates_remote'][remoteB[i].strip()][1]) + "</td>\n"
-        output_html += "</tr>\n"
-    output_html += "</tbody></table></div>\n"
+    # for i in range(0, len(remoteB)):
+    #     output_html += "<tr>\n"
+    #     output_html += "<td><a href=\"" + remoteB[i].split("/")[-1] + "_remote.html\">" + remoteB[i] + "</a></td>\n"
+    #     output_html += "<td>" + str(br_stats['branch_dates_remote'][remoteB[i].strip()][0]) + "</td>\n"
+    #     output_html += "<td>" + str(br_stats['branch_dates_remote'][remoteB[i].strip()][1]) + "</td>\n"
+    #     output_html += "</tr>\n"
+    # output_html += "</tbody></table></div>\n"
 
     output_html += "    <div class=\"row \">\n"
     output_html += "        <h2>Local Branches</h2>\n"
@@ -588,8 +574,8 @@ def generate_output(statistics, path):
     f.write(output_html)
     f.close()
     branch_stats_local = statistics["br_stats"]["branch_stats_local"]
-    branch_stats_remote = statistics["br_stats"]["branch_stats_remote"]
+    # branch_stats_remote = statistics["br_stats"]["branch_stats_remote"]
 
     generate_branch_history(path, branch_stats_local, "local")
-    generate_branch_history(path, branch_stats_remote, "remote")
+    # generate_branch_history(path, branch_stats_remote, "remote")
 
